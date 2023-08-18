946a38d39da906baa89a5d357999f55cb4a4774c
madstone290 <madstone290@gmail.com>
2023-08-18 15:46:20 +0900

    Chore 미사용 import 삭제

--- ReactMUI/src/App.tsx
+++ ReactMUI/src/App.tsx
@@ -1,6 +1,4 @@
-import { Typography } from '@mui/material';
 import './App.css'
-import Page1 from 'pages/Home'
 import RootTheme from 'theme/RootTheme';
 import { MyRouter } from 'routes/MyRouter';
 
